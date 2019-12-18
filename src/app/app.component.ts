7a7b9b47e0b4dec6f387d175ad36520a74c2dbff
pavel <pavel.luczencko2014@gmail.com>
2019-12-18 11:49:35 +0300

    yt hj,bn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+// tslint:disable-next-line: max-line-length
+import { Component, OnInit, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
+// tslint:disable-next-line:no-conflicting-lifecycle
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+// tslint:disable-next-line: max-line-length
+export class AppComponent implements OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, DoCheck {
   title = 'Library';
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges');
+  }
+  ngOnInit() {
+    console.log('OnInit');
+  }
+  ngDoCheck() {
+    console.log('DoCheck')
+  }
+  ngAfterContentInit() {
+    console.log('AfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked');
+  }
+  ngAfterViewInit() {
+    console.log('AfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy');
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 declare var $: any;
+// tslint:disable-next-line:no-conflicting-lifecycle
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  constructor() { }
-  namer: 'kek';
+// tslint:disable-next-line:max-line-length
+export class HeaderComponent implements OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, DoCheck {
+  constructor() {
 
+  }
+  registered: 0;
+  namer: 'kek';
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges1');
+  }
   ngOnInit() {
     // tslint:disable-next-line: only-arrow-functions
     $(document).ready(function () {
@@ -26,33 +33,60 @@ export class HeaderComponent implements OnInit {
         }
       }
       );
-
     });
+  }
+  ngDoCheck() {
+  }
+  ngAfterContentInit() {
+    console.log('AfterContentInit1');
+  }
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked1');
+  }
+  ngAfterViewInit() {
+    console.log('AfterViewInit1');
+  }
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked1');
     if (localStorage.getItem('loginname')) {
-
+      console.log('Есть ЛокалСторадж');
       const object = JSON.parse(localStorage.getItem('loginname'));
       const elem = 'name';
       const gender = 'gender';
       const namer = object[elem];
       const genderr = object[gender];
       if (genderr === 'Женский') {
+        console.log('Дивка');
         document.getElementById('loginwomanreg').classList.toggle('unshown');
       } else {
+        console.log('Поцан');
         document.getElementById('loginmanreg').classList.toggle('unshown');
       }
       document.getElementById('logined').innerHTML = namer;
+      console.log('Имя: ' + namer);
     }
-    window.addEventListener('load', function () {
-      if (localStorage.getItem('loginname')) {
-        if ($('#login')) {
-          document.getElementById('registered').classList.toggle('unshown');
-          $('#login').html(localStorage.getItem('loginname'));
-        }
+    let registered = 0;
+    if (localStorage.getItem('loginname')) {
+      if (registered === 1) {
+        document.getElementById('loginmanreg').classList.toggle('unshown');
+        document.getElementById('unregistered').classList.toggle('unshown');
+        registered++;
+        console.log(registered);
+      }
+      if ($('#login')) {
+        document.getElementById('registered').classList.toggle('unshown');
+        $('#login').html(localStorage.getItem('loginname'));
       } else {
         document.getElementById('unregistered').classList.toggle('unshown');
+        console.log(registered);
       }
-    });
+      registered++;
+    }
   }
+  ngOnDestroy() {
+    console.log('OnDestroy1');
+  }
+
 }
 
 
